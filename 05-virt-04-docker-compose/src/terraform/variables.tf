2abfdbf5af0604a72a5fa53b117e971e3dbe26da
gkublock <lisik51@gmail.com>
2022-11-01 23:53:41 +0300

    1st task

--- 05-virt-04-docker-compose/src/terraform/variables.tf
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -1,13 +1,13 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1g2j4dj6u8hmmp9vrip"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1gkni0qck1jvqip4dt2"
 }
 
 # Заменить на ID своего образа
